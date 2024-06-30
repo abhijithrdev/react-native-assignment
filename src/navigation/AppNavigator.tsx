da5e403d00224ea3a19206da4f9f4bf7cf51b562
abhijithrdev <rajagopalabhijith@gmail.com>
2024-06-30 23:04:35 +0530

    profile screen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,13 +1,33 @@
-import {View, Text} from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {LoginScreen} from '../screens';
 import HomeTabNavigator from './HomeTabNavigator';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { UserProfile } from '../types/UserProfile';
 
 const AppNavigator = () => {
   const Stack = createNativeStackNavigator();
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState<UserProfile>();
+
+  const getUserSignIn = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('@user_data');
+      const userObj = jsonValue != null ? JSON.parse(jsonValue) : null;
+      setUserData(userObj);
+      console.log('user data set in useeffect');
+      userObj.phoneVerified ? setIsLoggedIn(true) : setIsLoggedIn(false);
+    } catch (error) {
+      console.error('Error retrieving user sign-in data:', error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    getUserSignIn();
+  }, []);
 
   return (
     <NavigationContainer>
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,25 +1,101 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Switch, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {UserProfile} from '../types/UserProfile';
 
 const ProfileScreen = () => {
+  const [userData, setUserData] = useState<UserProfile>();
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const getUserSignIn = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('@user_data');
+      const userObj = jsonValue != null ? JSON.parse(jsonValue) : null;
+      userObj !== undefined ? setUserData(userObj) : null;
+      console.log('user data set in useeffect');
+    } catch (error) {
+      console.error('Error retrieving user sign-in data:', error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    getUserSignIn();
+  }, []);
+
+  useEffect(() => {
+    console.log('isEnabled', isEnabled);
+  }, [isEnabled]);
+
+  const toggleSwitch = () => {
+    setIsEnabled(previousState => !previousState);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.headerText}>ProfileScreen</Text>
-    </View>
-  )
-}
+      <View
+        style={{
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <Image source={{uri: userData?.photoURL}} style={styles.image} resizeMode='contain'/>
+        <Text style={styles.textName}>{userData?.displayName}</Text>
+        <Text style={styles.text}>Email: {userData?.email}</Text>
+        <Text style={styles.text}>Phone Number: {userData?.phoneNumber}</Text>
+      </View>
 
-export default ProfileScreen
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <Text style={styles.darkModeText}>Dark mode</Text>
+        <Switch
+          trackColor={{false: '#767577', true: '#81b0ff'}}
+          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+          style={{ transform:[{ scaleX: 1.3 }, { scaleY: 1.3 }] }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default ProfileScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems:'center',
-        justifyContent: 'center',
-    },
-    headerText: {
-        color: 'black',
-        fontSize: 50,
-        fontFamily: 'Fira Code Bold'
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+    justifyContent: 'space-between',
+paddingVertical: '40%'
+  },
+  image: {
+    width: 96,
+    height: 96,
+    borderRadius: 48,
+    marginBottom: 20,
+  },
+  textName: {
+    fontSize: 30,
+    marginBottom: 10,
+    fontFamily: 'Fira Code SemiBold',
+    color: 'black',
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 10,
+    fontFamily: 'Fira Code Regular',
+    color: 'black',
+  },
+  darkModeText: {
+    fontSize: 25,
+    fontFamily: 'Fira Code Medium',
+    color: 'black',
+    marginRight: '35%'
+  },
+});
